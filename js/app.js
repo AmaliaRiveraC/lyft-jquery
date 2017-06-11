41008fe4811acc76480647a3373ee67a79296760
Amalia Rivera Castillejos <amaliariveracastillejos@gmail.com>
2017-06-11 17:03:31 -0500

    Delete app.js duplicado en un commit

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var redirigir = function () {
-   window.location.href = "views/home.html";
- };
-
- var cargarPagina = function () {
-   setTimeout(redirigir, 3000);
- };
-
- $(document).ready(cargarPagina);
