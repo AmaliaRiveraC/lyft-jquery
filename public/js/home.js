b53c8dfec66f3bd974103abd251c01fad3c11953
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-06-12 00:34:16 -0500

    quedan ultimos detalles en validacion

--- public/js/home.js
+++ public/js/home.js
@@ -6,4 +6,42 @@ var cargarPagina = function() {
   $("#signup").click(redirigir);
 };
 
+var longitudTelefono = function() {
+	var longitud = $("#telefono").val().length;
+	if (longitud == 10) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+var validacion = function(evento) {
+	if (longitudTelefono()) {
+		$("#enviar").attr("href", "verify.html");
+		$(this).removeClass("alerta");
+	} else {
+		$("#siguiente").removeAttr("href");
+		$(this).addClass("alerta");
+	}
+};
+  
+		
+		$(this).removeClass("alerta");
+		localStorage.setItem("numCelular", $(this).val());
+	} else {
+		$("#siguiente").removeAttr("href");
+		$(this).addClass("alerta");
+	}
+};
+
+};
+function validarFormTel() {
+	if (inputTelefono.value.length == 8) {
+		btnInputTelefono.disabled = true;
+	}
+	else {
+		btnInputTelefono.disabled = false;
+	}
+}
+
 $(document).ready(cargarPagina);
