9e01203cfa339962fa4605dfca63021226099250
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-06-13 00:58:45 -0500

    redireccionando a cuarto html

--- public/js/home.js
+++ public/js/home.js
@@ -1,47 +1,12 @@
 var redirigir = function() {
-  window.location.href = "signout.html";
+  window.location.href = "codigo.html";
+  console.log($(this));
 };
 
 var cargarPagina = function() {
   $("#signup").click(redirigir);
 };
 
-var longitudTelefono = function() {
-	var longitud = $("#telefono").val().length;
-	if (longitud == 10) {
-		return true;
-	} else {
-		return false;
-	}
-};
 
-var validacion = function(evento) {
-	if (longitudTelefono()) {
-		$("#enviar").attr("href", "verify.html");
-		$(this).removeClass("alerta");
-	} else {
-		$("#siguiente").removeAttr("href");
-		$(this).addClass("alerta");
-	}
-};
-  
-		
-		$(this).removeClass("alerta");
-		localStorage.setItem("numCelular", $(this).val());
-	} else {
-		$("#siguiente").removeAttr("href");
-		$(this).addClass("alerta");
-	}
-};
-
-};
-function validarFormTel() {
-	if (inputTelefono.value.length == 8) {
-		btnInputTelefono.disabled = true;
-	}
-	else {
-		btnInputTelefono.disabled = false;
-	}
-}
 
 $(document).ready(cargarPagina);
